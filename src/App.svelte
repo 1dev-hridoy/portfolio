d7ef092b1eecf207cd3319b20da3e0b809506a45
dishan1223 <dishanishtiaq45+github@gmail.com>
2026-07-16 20:55:35 +0600

    fix: animation speed

--- src/App.svelte
+++ src/App.svelte
@@ -45,14 +45,14 @@ const works = [
 
 <main class="mx-auto w-[90%] max-w-[560px] mb-10 page-reveal">
 
-  <BlurFade delay={0.25}>
+  <BlurFade>
     <h1 class="mt-[50px] lg:mt-[100px] font-medium">Ishtiaq Dishan</h1>
   </BlurFade>
-  <BlurFade delay={0.45}>
+  <BlurFade>
     <p class="opacity-50">Updated 16 Jul 2026</p>
   </BlurFade>
 
-  <BlurFade delay={0.65}>
+  <BlurFade>
     <p class="mt-6">I'm a full-stack web developer from Sirajganj, Bangladesh, and a Class 12 student. I love building small, polished products that are simple, useful, and enjoyable to use.</p>
 
     <p class="mt-6">I've built products like <a href="https://www.studify.sbs/"><span class="link">Studify</span></a> and <a href="https://www.bdsaaszone.site/"><span class="link">BD SaaS Zone</span></a>, and I'm currently working on <a href="https://github.com/dishan1223/mutt"><span class="link">Mutt</span></a>, an open-source project. I'm always excited to build useful and enjoyable software while exploring new tools, technologies, and ideas. Beyond coding, I enjoy connecting with experienced developers, learning from their insights, and continuously improving my craft.</p>
@@ -60,7 +60,7 @@ const works = [
     <p class="mt-6">Feel free to say hi on <a href="https://github.com/dishan1223"><span class="link">GitHub</span></a>, <a href="https://x.com/ishtiaqdishan"><span class="link">X</span></a>, <a href="https://www.facebook.com/ishtiaq.dishan"><span class="link">Facebook</span></a>, or <a href="mailto:dishanishtiaq45@gmail.com"><span class="link">email</span></a> if you'd like to connect.</p>
   </BlurFade>
 
-  <BlurFade delay={0.85}>
+  <BlurFade>
   <Devider />
     <h1 class="mt-6 opacity-50">Tech Stack</h1>
     <p class="mt-4">I always try to pick the right tools for the job. These are the languages and technologies I use most often and enjoy working with.</p>
@@ -71,7 +71,8 @@ const works = [
     <p class="mt-4 whitespace-nowrap overflow-x-auto"><span class="opacity-50">Tools : </span> <Stack value="Git" /></p>
     <p class="mt-4 whitespace-nowrap overflow-x-auto"><span class="opacity-50">Design : </span> <Stack value="Figma" /></p>
     <p class="mt-4 whitespace-nowrap overflow-x-auto"><span class="opacity-50">Productivity : </span> <Stack value="Obsidian" /></p>
-
+  </BlurFade>
+  <BlurFade >
     <Devider />
     <h1 class="mt-6 opacity-50">Work</h1>
     {#each works as w}
